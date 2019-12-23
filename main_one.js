1344d6dcd08c381041bd26d14c6a362c0601b254
Stosportos <stas.alex.lapshin@gmail.com>
2019-12-23 14:09:02 +0300

    lesson5

--- main_one.js
+++
@@ -1,6 +0,0 @@
-const textBlock = document.querySelector('.text-content');
-const buttonReplacer = document.querySelector('.button-replacer');
-
-buttonReplacer.addEventListener('click', () => {
-    textBlock.textContent = textBlock.textContent.replace(/\B'|'\B/g, '"');
-});
